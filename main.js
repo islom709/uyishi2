c42c59814bac0d7a9a47b96201c34ab40907494f
islom709 <rislom709@gmail.com>
2025-03-01 14:03:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let body = document.querySelector('body')
+function dardMode() {
+    body.style.backgroundColor = "#000";
+    body.style.color = "#fff";
+}
+
+function lightMode() {
+    body.style.backgroundColor = "#fff";
+    body.style.color = "#000";
+
+}
